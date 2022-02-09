73f91635514ee26931cc5eb1e6d3df1fcb657af2
JaelMooh <99295436+JaelMooh@users.noreply.github.com>
2022-02-09 11:12:13 +0100

    Add files via upload

---
+++ AADF/index.js
@@ -0,0 +1,10 @@
+const drawerMenu = document.getElementById("drawer-menu");
+
+const openNav = () => {
+  drawerMenu.style.width = "350px";
+};
+
+const closeNav = () => {
+  drawerMenu.style.width = "0px";
+};
+
